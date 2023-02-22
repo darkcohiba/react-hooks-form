eac2815555a6a402c9f07be41d47b14e832869c9
Sam <samuel.waters@gmail.com>
2023-02-22 10:07:47 -0600

    chore: built out form according to db

--- src/pages/MovieForm.js
+++ src/pages/MovieForm.js
@@ -1,6 +1,46 @@
+import { useForm } from "react-hook-form";
+
+
 function MovieForm() {
+
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors }
+    } = useForm();
+
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
     return (
         <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>Movie Name:</label>
+                <input {...register("name", { required: true })} />
+                {errors.name && <p>This field is required</p>}
+                <br></br>
+                <label>Movie Genre:</label>
+                <input {...register("genre", { required: true })} />
+                {errors.genre && <p>This field is required</p>}
+                <br></br>
+                <label>Movie Release Year:</label>
+                <input {...register("releaseYear", { required: true, maxLength: 4 })} />
+                {errors.releaseYear && <p>This field is required and must be no longer then 4 numbers</p>}
+                <br></br>
+                <label>Movie Poster URL:</label>
+                <input {...register("PosterURL", { required: true })} />
+                {errors.PosterURL && <p>This field is required</p>}
+                <br></br>
+                <label>Movie Website:</label>
+                <input {...register("Website", { required: true })} />
+                {errors.Website && <p>This field is required</p>}
+                <br></br>
+                
+                <input type="submit" />
+            </form>
             
         </div>
     );
